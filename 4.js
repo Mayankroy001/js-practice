081272d65465db3f0c40b1f342232cef33f0818e
Mayankroy001 <157673205+Mayankroy001@users.noreply.github.com>
2026-04-15 02:37:49 +0530

    form submition day-1

---
+++ 4.js
@@ -0,0 +1,9 @@
+let form = document.querySelector('form');
+let input = document.querySelector('input');
+
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    console.log(inputs);
+    
+});
