587fb2f0cef3df1e321fd957c47949afc8f34a9d
Mayankroy001 <157673205+Mayankroy001@users.noreply.github.com>
2026-04-20 03:38:04 +0530

    card from event handling

--- 4.js
+++ 4.js
@@ -1,9 +1,8 @@
 let form = document.querySelector('form');
-let input = document.querySelector('input');
+let input = document.querySelectorAll('input');
+form.addEventListener("submit", function(dets) {
+    dets.preventDefault();
 
-
-form.addEventListener('submit', function(event) {
-    event.preventDefault();
-    console.log(inputs);
+    console.log(input[0].value, input[1].value, input[2].value);
     
-});
+    });
