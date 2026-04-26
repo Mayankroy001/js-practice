dd21c7399b92dfef51033c3214034354078ef832
Mayankroy001 <157673205+Mayankroy001@users.noreply.github.com>
2026-04-27 03:44:45 +0530

    dynamic card

--- revise/4.js
+++ revise/4.js
@@ -1,7 +1,35 @@
 let form = document.querySelector('form');
-let main = document.querySelector('main');
-form.addEventListener("submit", function(dets) {
-    dets.preventDefault();  
-    console.log(dets);
-    
-}); 
+let inputs = document. querySelectorAll('input');
+let main = document.querySelector('# main');
+
+
+form.addEventListener("submit", function(dets){
+    dets.preventDefault();
+
+
+    let card = document.createElement('div');
+card.classList.add('card');
+
+let profile = document.createElement('div');
+profile.classList.add('profile');
+
+let img = document.createElement('img');
+img.setAttribute('src', 'https://images.unsplash.com/photo-1761839257349-037aea1d94de?q=80&w=1169&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
+
+let h1 = document.createElement('h1');
+h1.textContent = "Rahul Roy";
+let h3 = document.createElement('h3');
+h3.textContent = "Web Developer";
+let p = document.createElement('p');
+p.textContent = "Gaar maro";
+
+
+profile.appendChild(img);
+card.appendChild(profile);
+card.appendChild(h1);
+card.appendChild(h3);
+card.appendChild(p);
+
+main.appendChild(card);
+});
+
