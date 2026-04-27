48e36a58b1b0b591f26f81e935aaab4b8f40d275
Mayankroy001 <157673205+Mayankroy001@users.noreply.github.com>
2026-04-27 21:53:19 +0530

    revise mouseover and dynamic card

---
+++ revise/mousemove.js
@@ -0,0 +1,8 @@
+let box = document.querySelector('.box');
+
+window.addEventListener('mousemove', function(dets){
+    console.log(dets);
+    box.style.top = dets.clientY + 'px';
+    box.style.left = dets.clientX + 'px';
+});
+
