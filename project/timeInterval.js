50a23c89c37ac350b17ba732bda3b83592f232a7
Mayankroy001 <157673205+Mayankroy001@users.noreply.github.com>
2026-05-12 02:49:14 +0530

    TimeIntervalProject

---
+++ project/timeInterval.js
@@ -0,0 +1,12 @@
+let ct = 20;
+
+let int = setInterval(function(){
+    if(ct>=1){
+        console.log(`ct: ${ct} - hello world`);
+        ct--
+    }
+
+    else{
+        clearInterval(int);
+        console.log("time up");}
+}, 2000);
